051c2a4a126edd67a65316b003a95c295ad337c2
Neider Hernández <neiderkk@gmail.com>
2023-01-12 15:18:40 -0500

    useCounter - CustomHook

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,17 @@
+import { useCounter } from "../hooks/useCounter"
+
+export const CounterWithCustomHook = () => {
+
+    const { counter } = useCounter();
+
+
+
+  return (
+    <>
+        <h1>Counter with Custom Hook: { counter } </h1>
+        <button className="btn btn-primary">+1</button>
+        <button className="btn btn-primary">-1</button>
+        <button className="btn btn-primary">Reset</button>
+    </>
+  )
+}
