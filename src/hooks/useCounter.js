83b82753ddfcf591de2ea8ba365f542e20b16945
Neider Hernández <neiderkk@gmail.com>
2023-01-12 18:28:15 -0500

    Exponer métodos del Hook

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -4,8 +4,23 @@ export const useCounter = ( inicialValue = 10 ) => {
 
     const [counter, setCounter] = useState(inicialValue);
 
+    const increment = (value) => {
+        setCounter(counter + value);
+    }
+
+    const decrement = (value) => {
+        if(counter === 0)return;
+        setCounter(counter - value);
+    }
+
+    const reset = () => {
+        setCounter(inicialValue);
+    }
 
     return{
         counter,
+        increment,
+        decrement,
+        reset,
     }
 }
