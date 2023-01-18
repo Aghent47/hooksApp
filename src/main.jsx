d2ddc972a51270cd37b80987a00fac2e60a554a6
Neider Hernández <neiderkk@gmail.com>
2023-01-18 07:27:12 -0500

    Continuación del Proyecto - HookApp

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,12 @@ import { Padre } from './07-tarea-memo/Padre';
 /* import { CounterApp } from './01-useState/CounterApp'; */
 /* import { HooksApp } from './HooksApp'; */
 import './index.css'
+import './08-useReducer/intro-reducer';
 /* import { UseLayoutEffect } from './useLayoutEffect/UseLayoutEffect'; */
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+/* ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
     <Padre/>
   //</React.StrictMode>,
 )
+ */
